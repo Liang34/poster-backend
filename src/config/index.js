04eb4ec92b43dcb20934632ded60007af3110d4c
liangjinahui <1160685230@qq.com>
2023-06-16 13:03:32 +0800

    feat: 修改环境变量

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@
  * @author liangjianhui
  */
 
-// const { isPrd, isPrdDev } = require('../utils/env')
+const { isPrd, isPrdDev } = require('../utils/env')
 
 // 获取各个环境的不同配置文件
 // let fileName = 'dev.js'
 // if (isPrdDev) fileName = 'prd-dev.js'
 // if (isPrd) fileName = 'prd.js'
 
-// const conf = require(`./envs/${fileName}`) // eslint-disable-line
-const conf = require(`./envs/dev.js`)
+const conf = require(`./envs/${fileName}`) // eslint-disable-line
+// const conf = require(`./envs/dev.js`)
 module.exports = conf
