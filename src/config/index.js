a6df5b12d1a8c45e2095feacbd3db4c47f3aad66
liangjinahui <1160685230@qq.com>
2023-06-17 18:54:39 +0800

    feat: 数据库设计

--- src/config/index.js
+++ src/config/index.js
@@ -6,9 +6,9 @@
 const { isPrd, isPrdDev } = require('../utils/env')
 
 // 获取各个环境的不同配置文件
-// let fileName = 'dev.js'
-// if (isPrdDev) fileName = 'prd-dev.js'
-// if (isPrd) fileName = 'prd.js'
+let fileName = 'dev.js'
+if (isPrdDev) fileName = 'prd-dev.js'
+if (isPrd) fileName = 'prd.js'
 
 const conf = require(`./envs/${fileName}`) // eslint-disable-line
 // const conf = require(`./envs/dev.js`)
